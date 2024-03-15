53b4cb5c5c2273db14f42d5a59c8b17c3516ef1d
Connor O'Hara <connorohara@connors-mbp.mynetworksettings.com>
2024-03-15 14:46:34 -0400

    full blob

--- program/src/main.rs
+++ program/src/main.rs
@@ -2,7 +2,7 @@
 #![no_main]
 sp1_zkvm::entrypoint!(main);
 
-use nmt_rs::{NamespaceId, NamespacedHash};
+use nmt_rs::{simple_merkle::proof, NamespaceId, NamespacedHash};
 
 use celestia_types::{nmt::Namespace, Blob};
 use serde::de::{self, Deserialize, Deserializer, SeqAccess, Visitor};
@@ -13,31 +13,53 @@ pub fn main() {
     // resulting in output that doesn't match fibonacci sequence.
     // However, the resulting proof will still be valid!
 
-    // Read shares
-    /*let mut shares: Vec<[u8; 512]> = Vec::new();
-    let mut buf = [0u8; 512];
-    for _ in 0..24 {
-        sp1_zkvm::io::read_slice(&mut buf);
-        shares.push(buf.clone());
-    };*/
-
-    // Read blob
-    //let blob: Blob = sp1_zkvm::io::read();
-
-    // read row root
-    let root = sp1_zkvm::io::read::<NamespacedHash<29>>();
-
+    // read original data square (ODS) size
+    let ods_size: u32 = sp1_zkvm::io::read();
+    // read num rows
+    let num_rows: u32 = sp1_zkvm::io::read();
     // read namespace ID
     let namespace = sp1_zkvm::io::read::<Namespace>();
 
-    // Read proof
-    let proof: celestia_types::nmt::NamespaceProof = sp1_zkvm::io::read();
-    let mut leaves = [[0u8; 512]; 24];
-    for i in 0..24 {
+    // read leaves
+    const num_leaves: usize = 272;
+    let mut leaves = [[0u8; 512]; num_leaves];
+    for i in 0..num_leaves {
         sp1_zkvm::io::read_slice(&mut leaves[i]);
     }
-    let result = proof.verify_range(&root, &leaves, namespace.into_inner());
-    sp1_zkvm::io::write(&result.is_ok());
+
+    let mut start = 0;
+    for i in 0..(num_rows as usize) {
+        let root = sp1_zkvm::io::read::<NamespacedHash<29>>();
+        let proof: celestia_types::nmt::NamespaceProof = sp1_zkvm::io::read();
+        let end = start + (proof.end_idx() as usize - proof.start_idx() as usize);
+        let result = proof.verify_range(&root, &leaves[start..end], namespace.into_inner());
+        println!("row {} result: {}", i, result.is_ok());
+        start = end;
+        if result.is_err() {
+            sp1_zkvm::io::write(&false);
+            return;
+        }
+    }
+
+    sp1_zkvm::io::write(&true);
+    // do middle rows
+
+    /*for i in 0..num_rows {
+        // read row root
+        let root = sp1_zkvm::io::read::<NamespacedHash<29>>();
+        // read proof
+        let proof: celestia_types::nmt::NamespaceProof = sp1_zkvm::io::read();
+        let result = proof.verify_range(
+            &root,
+            &leaves[..(proof.end_idx() as usize - proof.start_idx() as usize)],
+            namespace.into_inner(),
+        );
+        if result.is_err() {
+            sp1_zkvm::io::write(&false);
+            return;
+        }
+    }*/
+    //sp1_zkvm::io::write(&true);
 
     /*let n = sp1_zkvm::io::read::<u32>();
     let mut a: u128 = 0;
