a87031f47cd09f109081717047c861530d923987
c-node <chuckhead7@gmail.com>
2024-09-11 14:39:15 +0200

    mock

--- program/src/main.rs
+++ program/src/main.rs
@@ -1,28 +1,10 @@
 #![no_main]
 sp1_zkvm::entrypoint!(main);
 
-use celestia_types::{nmt::Namespace, Blob};
-use sha3::{Digest, Keccak256};
-
+// This is a dummy / mock program just to output the same type values as the real program
 pub fn main() {
-    // read the namespace
-    let namespace: Namespace = sp1_zkvm::io::read();
-
-    // Read the blob data
-    let data = sp1_zkvm::io::read_vec();
-
-    // Create a blob
-    let blob = Blob::new(namespace, data).expect("Failed to create blob");
-
-    // Compute the commitment
-    let commitment = blob.commitment;
-
-    // Compute the keccak256 hash of the blob
-    let mut hasher = Keccak256::new();
-    hasher.update(&blob.data);
-    let hash = hasher.finalize();
-
-    // Output the commitment bytes
-    sp1_zkvm::io::commit_slice(&commitment.0);
+    let commitment = [0u8; 32];
+    let hash = [0u8; 32];
+    sp1_zkvm::io::commit_slice(&commitment);
     sp1_zkvm::io::commit_slice(&hash);
 }
